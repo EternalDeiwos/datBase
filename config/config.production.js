5d7469f4c66209b2bed00c09493beadbe38cb8f3
Greg Linklater <greglink49@gmail.com>
2019-06-24 13:21:24 +0200

    feat(config.production): correct auth key provisioning

--- config/config.production.js
+++ config/config.production.js
@@ -27,6 +27,7 @@ function process_env (input, def) {
   }
 }
 
+const data = process_env(process.env.DATA_DIR, '/data')
 const whitelist_file = process_env(process.env.WHITELIST_FILE, 'whitelist.txt')
 
 if (process_env(process.env.WHITELIST)) {
@@ -34,18 +35,21 @@ if (process_env(process.env.WHITELIST)) {
   fs.writeFileSync(path.join(__dirname, whitelist_file), whitelist.join('\n'), { mode: 0o600 })
 }
 
+fs.writeFileSync(path.join(data, 'pubkey.pem'), process.env.TOWNSHIP_PUB_KEY, { mode: 0o640 })
+fs.writeFileSync(path.join(data, 'prvkey.pem'), process.env.TOWNSHIP_PRV_KEY, { mode: 0o400 })
+
 module.exports = {
-  data: process_env(process.env.DATA_DIR, '/data'),
+  data,
   mixpanel: process_env(process.env.MIXPANEL_ENABLE, false) ? process.env.MIXPANEL_KEY : undefined,
   township: {
     secret: process.env.TOWNSHIP_SECRET,
     db: process_env(process.env.TOWNSHIP_DB, 'township.db'),
-    publicKey: process.env.TOWNSHIP_PUB_KEY,
-    privateKey: process.env.TOWNSHIP_PRV_KEY,
+    publicKey: 'pubkey.pem',
+    privateKey: 'prvkey.pem',
     algorithm: process_env(process.env.TOWNSHIP_DSA_JWA, 'ES512'),
   },
   email: {
-    from: 'noreply@dat.home.gryphus.io',
+    from: process_env(process.env.SMTP_FROM, 'noreply@datbase.org'),
     smtpConfig: !process_env(process.env.SMTP_ENABLE, false) ? undefined : {
       host: process_env(process.env.SMTP_HOST, 'smtp.postmarkapp.com'),
       port: process_env(process.env.SMTP_PORT, 2525),
